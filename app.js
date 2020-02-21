93854241f82c7ccfe71e1e4b7c7c14e3ec310f5e
Zach Taylor <zbtaylor1@gmail.com>
2020-02-20 23:56:49 -0500

    added basic error handler to bottom of app.js

--- app.js
+++ app.js
@@ -23,4 +23,16 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/api/auth", authRouter);
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  console.log(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error");
+});
+
 module.exports = app;
