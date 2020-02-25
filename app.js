1396e639dc7a1dceaa3516990ce0464a18f77ce3
Zach Taylor <zbtaylor1@gmail.com>
2020-02-24 20:35:12 -0500

    updated auth routes to remove the /api/

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/api/auth", authRouter);
+app.use("/auth", authRouter);
 
 // error handler
 app.use(function(err, req, res, next) {
