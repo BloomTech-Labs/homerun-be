cc5146489bfd3844e55ca1cf624ffe76f10af3f2
Zach Taylor <zbtaylor1@gmail.com>
2020-02-24 20:37:34 -0500

    removed as of yet un-needed package require

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const Users = require("../models/user-model.js");
 const { generateToken } = require("../middleware/token.js");
