01efe5318dd88c9420b17114d82e4c5b4f622a82
Zach Taylor <zbtaylor1@gmail.com>
2020-02-25 23:51:44 -0500

    fixed await -> async

--- data/seeds/02-members.js
+++ data/seeds/02-members.js
@@ -1,28 +1,27 @@
-exports.seed = await function(knex) {
+exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex.raw("TRUNCATE TABLE members RESTART IDENTITY CASCADE");
-  return knex("members")
-    .then(() => {
-      // Inserts seed entries
-      return knex("members").insert([
-        {
-          current_household: "a12345",
-          username: "test",
-          email: "test1@test.com",
-          password: "test1234",
-          token: "examplegoogletoken",
-          child: false,
-          points: 25
-        },
-        {
-          current_household: "a12345",
-          username: "sample",
-          email: "test2@test.com",
-          password: "test1234",
-          token: "examplegoogletoken",
-          child: true,
-          points: 40
-        }
-      ]);
-    });
+  return knex("members").then(() => {
+    // Inserts seed entries
+    return knex("members").insert([
+      {
+        current_household: "a12345",
+        username: "test",
+        email: "test1@test.com",
+        password: "test1234",
+        token: "examplegoogletoken",
+        child: false,
+        points: 25
+      },
+      {
+        current_household: "a12345",
+        username: "sample",
+        email: "test2@test.com",
+        password: "test1234",
+        token: "examplegoogletoken",
+        child: true,
+        points: 40
+      }
+    ]);
+  });
 };
