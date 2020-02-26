82a9ba4fb98bf85320c32ca9698f33e6940a6a42
Zach Taylor <zbtaylor1@gmail.com>
2020-02-25 22:34:00 -0500

    removed ids from members seed to make postgres happy, and changed their emails to be unique

--- data/seeds/02-members.js
+++ data/seeds/02-members.js
@@ -1,31 +1,28 @@
-
-
-exports.seed = function (knex) {
+exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('members').del()
+  return knex("members")
+    .del()
     .then(() => {
       // Inserts seed entries
-      return knex('members').insert([
+      return knex("members").insert([
         {
-          id: 1,
-          current_household: 'a12345',
-          username: 'test',
-          email: "test@test.com",
+          current_household: "a12345",
+          username: "test",
+          email: "test1@test.com",
           password: "test1234",
           token: "examplegoogletoken",
           child: false,
           points: 25
         },
         {
-          id: 2,
-          current_household: 'a12345',
-          username: 'sample',
-          email: "test@test.com",
+          current_household: "a12345",
+          username: "sample",
+          email: "test2@test.com",
           password: "test1234",
           token: "examplegoogletoken",
           child: true,
           points: 40
-        },
+        }
       ]);
     });
 };
