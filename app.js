1fe06e8cd59b13fe22eaf3573ea8358d675180d7
Zach Taylor <zbtaylor1@gmail.com>
2020-02-26 22:43:40 -0500

    added cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const todosRouter = require("./routes/todos-router.js");
@@ -16,6 +17,7 @@ const membersRouter = require("./routes/members-router.js");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
