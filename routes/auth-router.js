fdf93b3ae5753b9be611d3c318ea8c270f12ebea
Vinni Hoke <vinni.hoke@gmail.com>
2020-02-27 21:17:45 -0800

    Created a cryptographic system and user profile

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -2,12 +2,48 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const Members = require("../models/members-model.js");
 const { generateToken } = require("../middleware/token.js");
+const axios = require('axios');
 
 router.get('/hello', (req, res) => {
-  console.log("This is the req", req.session)
-  // todo: Perform auth check if they are in the database. Modify the email string to get a username, add to database if new user.
-  res.status(200).json(req.session).end()
+  try {
+    // How to get events from Google Calendar
+    // const events = await axios.get('https://www.googleapis.com/calendar/v3/users/me/calendarList', {
+    // 	headers: {
+    // 		Authorization: "OAuth <access_token>"
+    // 	}
+    // })
+
+    // todo: Need to store...
+    // ! Requires encryption
+    // Location for access_token: req.session.grant.response.access_token
+    // Location for refresh_token: req.session.grant.response.refresh_token
+    let accessHash = bcrypt.hashSync(req.session.grant.response.access_token, 14)
+    let refreshHash = bcrypt.hashSync(req.session.grant.response.refresh_token, 14)
+
+    // ? No encryption needed
+    // Location for email: req.session.grant.response.id_token.payload.email
+    // Create username with email: email.split('@')[0]
+    // Location for provider: req.session.grant.provider
+
+    const user = {
+      provider: req.session.grant.provider,
+      email: req.session.grant.response.id_token.payload.email,
+      username: req.session.grant.response.id_token.payload.email.split('@')[0],
+      access_token: accessHash,
+      refresh_token: refreshHash
+    }
+
+
+
+
+    console.log("This is the req", req)
+    res.status(200).json(req.session.grant)
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
 })
+
+
 
 router.post("/signup", async (req, res, next) => {
   const newMember = req.body;
