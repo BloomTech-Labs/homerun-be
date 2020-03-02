b1837e768b51fae8d16a72ef975a73779115c76d
Zach Taylor <zbtaylor1@gmail.com>
2020-03-01 23:48:24 -0500

    updated migration to add confirmations table

--- data/migrations/20200218211829_home-run-data.js
+++ data/migrations/20200218211829_home-run-data.js
@@ -1,99 +1,127 @@
-
-exports.up = function (knex) {
-	return knex.schema
-		.createTable('households', col => {
-			col.varchar('id', 7).notNullable().unique()
-			col.varchar('title', 40)
-			col.integer('pin', 4)
-		})
-		.createTable('members', col => {
-			col.increments()
-			col.varchar('username', 40).notNullable().unique()
-			col.varchar('email', 128).notNullable().unique()
-			col.text('password')
-			col.text('provider')
-			col.text('access_token')
-			col.text('refresh_token')
-			col.boolean('child')
-			col.integer('points')
-			col.varchar('current_household')
-				.unsigned()
-				.references('households.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-		})
-		.createTable('todos', col => {
-			col.increments()
-			col.text('household').notNullable()
-			col.varchar('title', 40).notNullable()
-			col.varchar('desc', 255)
-			col.integer('point_value')
-			col.bigint('due')
-			col.boolean('completed')
-			col.text('completed_by')
-		})
-		.createTable('rewards', col => {
-			col.increments()
-			col.text('title').notNullable()
-			col.integer('point_total').notNullable()
-			col.varchar('household_id')
-				.unsigned()
-				.references('households.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-		})
-		.createTable('inventory', col => {
-			col.increments()
-			col.varchar('household_id')
-				.unsigned()
-				.references('households.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-		})
-		.createTable('bills', col => {
-			col.increments()
-			col.varchar('household_id')
-				.unsigned()
-				.references('households.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-		})
-		.createTable('household_members', col => {
-			col.increments()
-			col.integer('member_id')
-				.unsigned()
-				.references('members.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-			col.varchar('household_id')
-				.unsigned()
-				.references('households.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-		})
-		.createTable('todos_members', col => {
-			col.increments()
-			col.integer('members_id')
-				.unsigned()
-				.references('members.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-			col.integer('todos_id')
-				.unsigned()
-				.references('todos.id')
-				.onDelete('CASCADE')
-				.onUpdate('CASCADE')
-		})
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("households", col => {
+      col
+        .varchar("id", 7)
+        .notNullable()
+        .unique();
+      col.varchar("title", 40);
+      col.integer("pin", 4);
+    })
+    .createTable("members", col => {
+      col.increments();
+      col
+        .varchar("username", 40)
+        .notNullable()
+        .unique();
+      col
+        .varchar("email", 128)
+        .notNullable()
+        .unique();
+      col.text("password");
+      col.text("provider");
+      col.text("access_token");
+      col.text("refresh_token");
+      col.boolean("child");
+      col.integer("points");
+      col
+        .varchar("current_household")
+        .unsigned()
+        .references("households.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("todos", col => {
+      col.increments();
+      col.text("household").notNullable();
+      col.varchar("title", 40).notNullable();
+      col.varchar("desc", 255);
+      col.integer("point_value");
+      col.bigint("due");
+      col.boolean("completed");
+      col.text("completed_by");
+    })
+    .createTable("rewards", col => {
+      col.increments();
+      col.text("title").notNullable();
+      col.integer("point_total").notNullable();
+      col
+        .varchar("household_id")
+        .unsigned()
+        .references("households.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("inventory", col => {
+      col.increments();
+      col
+        .varchar("household_id")
+        .unsigned()
+        .references("households.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("bills", col => {
+      col.increments();
+      col
+        .varchar("household_id")
+        .unsigned()
+        .references("households.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("household_members", col => {
+      col.increments();
+      col
+        .integer("member_id")
+        .unsigned()
+        .references("members.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      col
+        .varchar("household_id")
+        .unsigned()
+        .references("households.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("todos_members", col => {
+      col.increments();
+      col
+        .integer("members_id")
+        .unsigned()
+        .references("members.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      col
+        .integer("todos_id")
+        .unsigned()
+        .references("todos.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("confirmations", col => {
+      col.increments("id");
+      col.text("hash");
+      col
+        .integer("member_id")
+        .unsigned()
+        .notNullable()
+        .references("members.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
-exports.down = function (knex) {
-	return knex.schema
-		.dropTableIfExists('todos_members')
-		.dropTableIfExists('household_members')
-		.dropTableIfExists('bills')
-		.dropTableIfExists('inventory')
-		.dropTableIfExists('rewards')
-		.dropTableIfExists('todos')
-		.dropTableIfExists('members')
-		.dropTableIfExists('households')
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("confirmations")
+    .dropTableIfExists("todos_members")
+    .dropTableIfExists("household_members")
+    .dropTableIfExists("bills")
+    .dropTableIfExists("inventory")
+    .dropTableIfExists("rewards")
+    .dropTableIfExists("todos")
+    .dropTableIfExists("members")
+    .dropTableIfExists("households");
 };
