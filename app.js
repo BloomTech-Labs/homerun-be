1c5d51d9ea175ef54da12e537fb1c11b5c2a54e2
Zach Taylor <zbtaylor1@gmail.com>
2020-03-02 22:40:43 -0500

    changed session host to env var

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const session = require('express-session');
-const grant = require('grant-express');
+const session = require("express-session");
+const grant = require("grant-express");
 
 const indexRouter = require("./routes/index");
 const todosRouter = require("./routes/todos-router.js");
@@ -27,31 +27,44 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // OAuth Setup
-app.use(session({ secret: process.env.SESSION_KEY, saveUninitialized: true, resave: true }))
-app.use(grant({
-  defaults: {
-    protocol: 'http',
-    host: 'localhost:3000',
-    transport: "session",
-    state: true
-  },
-  google: {
-    key: process.env.G_CLIENT_ID,
-    secret: process.env.G_CLIENT_SECRET,
-    scope: ['profile', 'email', 'openid', 'https://www.googleapis.com/auth/calendar'],
-    nonce: true,
-    custom_params: { "access_type": "offline" },
-    callback: "/auth/hello"
-  },
-  facebook: {
-    key: process.env.F_CLIENT_ID,
-    secret: process.env.F_CLIENT_SECRET,
-    scope: ['profile'],
-    nonce: true,
-    custom_params: { "access_type": "offline" },
-    callback: "/auth/hello"
-  },
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    saveUninitialized: true,
+    resave: true
+  })
+);
+app.use(
+  grant({
+    defaults: {
+      protocol: "http",
+      host: process.env.BASE_URL,
+      transport: "session",
+      state: true
+    },
+    google: {
+      key: process.env.G_CLIENT_ID,
+      secret: process.env.G_CLIENT_SECRET,
+      scope: [
+        "profile",
+        "email",
+        "openid",
+        "https://www.googleapis.com/auth/calendar"
+      ],
+      nonce: true,
+      custom_params: { access_type: "offline" },
+      callback: "/auth/hello"
+    },
+    facebook: {
+      key: process.env.F_CLIENT_ID,
+      secret: process.env.F_CLIENT_SECRET,
+      scope: ["profile"],
+      nonce: true,
+      custom_params: { access_type: "offline" },
+      callback: "/auth/hello"
+    }
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/todos", todosRouter);
@@ -59,7 +72,7 @@ app.use("/auth", authRouter);
 app.use("/members", membersRouter);
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
