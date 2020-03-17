63d8768e6417dd171fa1ff11bbafc3980037a275
Zach Taylor <zbtaylor1@gmail.com>
2020-03-16 22:18:46 -0400

    added household invite endpoint, prefixed other endpoints to avoid short circuit

--- routes/members-router.js
+++ routes/members-router.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
 const Members = require("../models/members-model.js");
 
-router.get("/:householdId", async (req, res) => {
+router.get("/household/:householdId", async (req, res) => {
   try {
     const members = await Members.findHouseholdMembers(req.params.householdId);
-    const children = await Members.childrenPerHousehold(req.params.householdId)
+    const children = await Members.childrenPerHousehold(req.params.householdId);
     for (let member of members) {
-      member.children = children
+      member.children = children;
     }
-    console.log(members)
     res.status(200).json(members);
   } catch (err) {
     res
@@ -17,14 +16,41 @@ router.get("/:householdId", async (req, res) => {
   }
 });
 
-router.get("/:householdId/assign", async (req, res) => {
+router.get("/household/:householdId/assign", async (req, res) => {
   try {
-    const members = await Members.totalHouseholdMembers(req.params.householdId)
-    const children = await Members.totalHouseholdChildren(req.params.householdId)
-    res.status(200).json([...members, ...children])
-  } catch (err) {
+    const members = await Members.totalHouseholdMembers(req.params.householdId);
+    const children = await Members.totalHouseholdChildren(
+      req.params.householdId
+    );
+    res.status(200).json([...members, ...children]);
+  } catch (err) {}
+});
 
+router.post("/invite", async (req, res, next) => {
+  const { email } = req.body;
+  if (email) {
+    // 1. Try to find member by email in database
+    Members.getByEmail(email)
+      .then(member => {
+        const newConfirmation = {
+          member_id: member.id,
+          hash: crypto.randomBytes(20).toString("hex")
+        };
+        Confirmations.insert(newConfirmation).then(hash => {
+          sendMail(member.email, templates.householdInvite(hash));
+          res.status(200).json({
+            message: `An invitation email has been sent to ${member.email}`
+          });
+        });
+      })
+      .catch(err => {
+        res
+          .status(400)
+          .json({ message: "A user with that email address does not exist." });
+      });
+  } else {
+    res.status(400).json({ message: "Please enter an email address." });
   }
-})
+});
 
 module.exports = router;
