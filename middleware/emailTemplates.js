5bd509be995b8ec088749b142ae09e3f31b480ca
Zach Taylor <zbtaylor1@gmail.com>
2020-03-16 22:38:13 -0400

    added household invite template and updated links in the text versions to match html versions

--- middleware/emailTemplates.js
+++ middleware/emailTemplates.js
@@ -4,15 +4,22 @@ module.exports = {
   confirmation: hash => {
     return {
       subject: "Email Confirmation - Tidy Hive",
-      text: `Please paste the following link in to your browser's URL bar in order to complete your registration: ${process.env.FE_URL}/confirm/${hash}`,
+      text: `Please paste the following link in to your browser's URL bar in order to complete your registration: ${process.env.FE_URL}/confirm-account/${hash}`,
       html: `Please follow <a href="${process.env.FE_URL}/confirm-account/${hash}">this link</a> to complete your registration.`
     };
   },
   reset: hash => {
     return {
       subject: "Password Reset - Tidy Hive",
-      text: `Please paste the following link in to your browser's URL bar in order to reset your password: ${process.env.FE_URL}/reset/${hash}`,
+      text: `Please paste the following link in to your browser's URL bar in order to reset your password: ${process.env.FE_URL}/reset-password/${hash}`,
       html: `Please follow <a href="${process.env.FE_URL}/reset-password/${hash}">this link</a> to reset your password.`
+    };
+  },
+  householdInvite: (hash, householdId) => {
+    return {
+      subject: "Household Invite - Tidy Hive",
+      text: `Please paste the following link in to your browser's URL bar in order to join this household: ${process.env.FE_URL}/invite/${hash}/${householdId}`,
+      html: `Please follow <a href="${process.env.FE_URL}/reset-password/${hash}/${householdId}">this link</a> to join this household.`
     };
   }
 };
