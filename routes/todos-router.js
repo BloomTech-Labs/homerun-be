7525c6b88ace3e5779f766330eed52d127b596c8
Micah <micahjank@gmail.com>
2020-04-19 16:07:55 -0500

    imported Categories model and used the findTodoCategories function inside some of the routes so that todos now return the categories that they belong to

--- routes/todos-router.js
+++ routes/todos-router.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const Todos = require("../models/todos-model.js");
 const TodosMembers = require("../models/todos-members-model.js");
 const TodosChildren = require("../models/todos-children-model.js");
+const Categories = require('../models/categories-model.js');
+
+const categoriesRouter = require('./categories-router.js');
 
 router.get("/household", async (req, res) => {
   const householdId = req.decodedToken.current_household;
@@ -14,10 +17,13 @@ router.get("/household", async (req, res) => {
       todosPerHousehold.map(async (todo) => {
         const membersAssigned = await Todos.findMembersAssigned(todo.id);
         const childrenAssigned = await Todos.findChildrenAssigned(todo.id);
+        // findTodoCategories should return an array with all the categories the todo belongs to
+        const todoCategories = await Categories.findTodoCategories(todo.id);
+        console.log(todoCategories);
         if (!membersAssigned && !childrenAssigned) {
-          return { ...todo, assigned: [] };
+          return { ...todo, assigned: [], categories: todoCategories };
         }
-        return { ...todo, assigned: membersAssigned.concat(childrenAssigned) };
+        return { ...todo, assigned: membersAssigned.concat(childrenAssigned), categories: todoCategories };
       })
     );
     res.status(200).json(allTodos);
@@ -35,10 +41,12 @@ router.get("/member", async (req, res) => {
       todosByMember.map(async (todo) => {
         const membersAssigned = await Todos.findMembersAssigned(todo.id);
         const childrenAssigned = await Todos.findChildrenAssigned(todo.id);
+        const todoCategories = await Categories.findTodoCategories(todo.id);
+
         if (!membersAssigned && !childrenAssigned) {
-          return { ...todo, assigned: [] };
+          return { ...todo, assigned: [], categories: todoCategories };
         }
-        return { ...todo, assigned: membersAssigned.concat(childrenAssigned) };
+        return { ...todo, assigned: membersAssigned.concat(childrenAssigned), categories: todoCategories };
       })
     );
     res.status(200).json(allTodos);
@@ -59,12 +67,15 @@ router.get("/child/:id", async (req, res) => {
         todosByChild.map(async (todo) => {
           const membersAssigned = await Todos.findMembersAssigned(todo.id);
           const childrenAssigned = await Todos.findChildrenAssigned(todo.id);
+          const todoCategories = await Categories.findTodoCategories(todo.id);
+
           if (!membersAssigned && !childrenAssigned) {
-            return { ...todo, assigned: [] };
+            return { ...todo, assigned: [], categories: todoCategories };
           }
           return {
             ...todo,
             assigned: membersAssigned.concat(childrenAssigned),
+            categories: todoCategories
           };
         })
       );
@@ -147,7 +158,11 @@ router.post("/add", (req, res, next) => {
     // TODO: Confirm that the household id is valid?
     Todos.insert(newTodo)
       .then((todo) => {
-        res.status(200).json({ ...todo, assigned: [] });
+        Categories.findTodoCategories(todo.id)
+        .then(categories => {
+          res.status(200).json({ ...todo, assigned: [], categories });
+        })
+
       })
       .catch((err) => {
         next(err);
@@ -166,7 +181,9 @@ router.put("/:id", (req, res, next) => {
       const membersAssigned = await Todos.findMembersAssigned(req.params.id);
       const childrenAssigned = await Todos.findChildrenAssigned(req.params.id);
       const assigned = Object.assign(membersAssigned, childrenAssigned);
-      res.status(200).json({ ...todo, assigned: assigned });
+      const todoCategories = await Categories.findTodoCategories(todo.id);
+
+      res.status(200).json({ ...todo, assigned: assigned, categories: todoCategories });
     })
     .catch((err) => {
       next(err);
@@ -184,12 +201,15 @@ router.delete("/:id", (req, res, next) => {
         householdTodos.map(async (todo) => {
           const membersAssigned = await Todos.findMembersAssigned(todo.id);
           const childrenAssigned = await Todos.findChildrenAssigned(todo.id);
+          const todoCategories = await Categories.findTodoCategories(todo.id);
+
           if (!membersAssigned && !childrenAssigned) {
-            return { ...todo, assigned: [] };
+            return { ...todo, assigned: [], categories: todoCategories };
           }
           return {
             ...todo,
             assigned: membersAssigned.concat(childrenAssigned),
+            categories: todoCategories
           };
         })
       );
@@ -199,5 +219,12 @@ router.delete("/:id", (req, res, next) => {
       next(err);
     });
 });
+
+
+// sub routers - rather than pile even more code in todos-router i just decided
+// to make categories routes have their own router file - since
+// categories-router will be a sub route of todos-router all routes inside it will
+// need to be prepended with '/todos'
+router.use('/categories', categoriesRouter);
 
 module.exports = router;
