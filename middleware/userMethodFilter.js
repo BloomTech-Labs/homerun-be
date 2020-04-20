42af14a8b92a847621d93eb4049f93078080eae3
Vinni Hoke <vinni.hoke@gmail.com>
2020-04-19 21:18:16 -0700

    Merged and cleaned up. Out of sync assign/unassign

--- middleware/userMethodFilter.js
+++ middleware/userMethodFilter.js
@@ -1,6 +1,22 @@
 const TodosMembers = require("../models/todos-members-model.js");
 const TodosChildren = require("../models/todos-children-model.js");
 
+const insert = async (user, id) => {
+	if (user.type === "child") {
+		await TodosChildren.insert({ child_id: user.id, todo_id: id });
+	} else {
+		await TodosMembers.insert({ member_id: user.id, todo_id: id });
+	}
+}
+
+const remove = async (user, id) => {
+	if (user.type === "child") {
+		await TodosChildren.remove({ child_id: user.id, todo_id: id });
+	} else {
+		await TodosMembers.remove({ member_id: user.id, todo_id: id });
+	}
+}
+
 module.exports = async (req, res, next) => {
 	if (!req.body || !req.params.id) console.log("Missing body or todo id")
 	const user = req.body;
@@ -8,11 +24,7 @@ module.exports = async (req, res, next) => {
 	switch (req.path) {
 		case `/assign/${id}`:
 			try {
-				if (user.type === "child") {
-					await TodosChildren.insert({ child_id: user.id, todo_id: id });
-				} else {
-					await TodosMembers.insert({ member_id: user.id, todo_id: id });
-				}
+				insert(user, id)
 				next()
 			} catch (e) {
 				console.log(e.message)
@@ -20,11 +32,7 @@ module.exports = async (req, res, next) => {
 			break;
 		case `/unassign/${id}`:
 			try {
-				if (user.type === "child") {
-					await TodosChildren.remove({ child_id: user.id, todo_id: id });
-				} else {
-					await TodosMembers.remove({ member_id: user.id, todo_id: id });
-				}
+				remove(user, id)
 				next()
 			} catch (e) {
 				console.log(e.message)
