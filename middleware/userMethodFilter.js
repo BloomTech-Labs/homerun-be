8712f3d2b423fd59e731268f6dda980da904ae46
Vinni Hoke <vinni.hoke@gmail.com>
2020-04-19 20:46:07 -0700

    Abstracted functionality to Middleware.

--- middleware/userMethodFilter.js
+++ middleware/userMethodFilter.js
@@ -3,33 +3,57 @@ const TodosChildren = require("../models/todos-children-model.js");
 
 module.exports = async (req, res, next) => {
 	if (!req.body || !req.params.id) console.log("Missing body or todo id")
-
 	const user = req.body;
 	const id = req.params.id;
+	// if (req.path === `/assign/${id}`) {
+	// 	try {
+	// 		if (user.type === "child") {
+	// 			await TodosChildren.insert({ child_id: user.id, todo_id: id });
+	// 		} else {
+	// 			await TodosMembers.insert({ member_id: user.id, todo_id: id });
+	// 		}
+	// 		next()
+	// 	} catch (e) {
+	// 		console.log(e.message)
+	// 	}
+	// } else {
+	// 	try {
+	// 		if (user.type === "child") {
+	// 			await TodosChildren.remove({ child_id: user.id, todo_id: id });
+	// 		} else {
+	// 			await TodosMembers.remove({ member_id: user.id, todo_id: id });
+	// 		}
+	// 		next()
+	// 	} catch (e) {
+	// 		console.log(e.message)
+	// 	}
+	// }
 
-	console.log(req.path)
+	switch (req.path) {
+		case `/assign/${id}`:
+			try {
+				if (user.type === "child") {
+					await TodosChildren.insert({ child_id: user.id, todo_id: id });
+				} else {
+					await TodosMembers.insert({ member_id: user.id, todo_id: id });
+				}
+				next()
+			} catch (e) {
+				console.log(e.message)
+			}
+		case `/unassign/${id}`:
+			try {
+				if (user.type === "child") {
+					await TodosChildren.remove({ child_id: user.id, todo_id: id });
+				} else {
+					await TodosMembers.remove({ member_id: user.id, todo_id: id });
+				}
+				next()
+			} catch (e) {
+				console.log(e.message)
+			}
+		default:
+			console.log("Invalid request")
 
-	if (req.path === `/assign/${id}`) {
-		try {
-			if (user.type === "child") {
-				await TodosChildren.insert({ child_id: user.id, todo_id: id });
-			} else {
-				await TodosMembers.insert({ member_id: user.id, todo_id: id });
-			}
-			next()
-		} catch (e) {
-			console.log(e.message)
-		}
-	} else {
-		try {
-			if (user.type === "child") {
-				await TodosChildren.remove({ child_id: user.id, todo_id: id });
-			} else {
-				await TodosMembers.remove({ member_id: user.id, todo_id: id });
-			}
-			next()
-		} catch (e) {
-			console.log(e.message)
-		}
 	}
 }
