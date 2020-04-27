b785969877f2929aca6077f53adcaaf94532847f
Micah <micahjank@gmail.com>
2020-04-27 14:34:37 -0500

    added dependency cross-env and updated scripts to reflect its usage - test should run now

--- __tests__/categories-router.test.js
+++ __tests__/categories-router.test.js
@@ -2,14 +2,13 @@ const request = require('supertest');
 
 const server = require('../app.js');
 
-const db = require('../data/dbConfig.js');
+const db = require('../db/dbConfig.js');
 
 
 // some of the routes that need to be tested require a token sent in the header - to get that i will login with the mom account
 // before any of the tests happen and store the token in a variable
 let token;
 beforeAll(done => {
-    console.log(process.env.NODE_ENV);
     request(server).post('/auth/login').send({ email: 'mom@test.com', password: 'test1234' })
     .end((err, response) => {
         token = response.body.token;
