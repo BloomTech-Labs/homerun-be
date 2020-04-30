35ae641fc0121e5c4ded6723fce6294fabfad228
Vinni Hoke <vinni.hoke@gmail.com>
2020-04-29 20:34:22 -0700

    Adding more tests to userMethodFilter

--- __tests__/userMethodFilter.test.js
+++ __tests__/userMethodFilter.test.js
@@ -4,21 +4,6 @@ const app = require('../app.js');
 
 // jest.mock('../middleware/userMethodFilter.js', () => jest.fn((req, res, next) => next()))
 
-const child = {
-	id: 1,
-	household_id: "a12345",
-	username: "Bobby",
-	child: true,
-	points: 60,
-}
-
-const user = {
-	id: 1,
-	username: "john",
-	email: "test@gmail.com",
-	password: "aslasdf123456asdf321a6s5df46"
-}
-
 let agent;
 let server;
 beforeEach(done => {
@@ -34,7 +19,14 @@ afterEach(done => {
 	server && server.close(done);
 });
 
-describe("Middleware functionality", () => {
+describe("Users", () => {
+	const user = {
+		id: 1,
+		username: "john",
+		email: "test@gmail.com",
+		password: "aslasdf123456asdf321a6s5df46"
+	}
+
 	test("Insert is called from the middleware", () => {
 		const req = {
 			body: user,
@@ -55,6 +47,45 @@ describe("Middleware functionality", () => {
 			path: `/unassign/${user.id}`,
 			params: {
 				id: user.id,
+			}
+		}
+		agent.post(req.path)
+			.send(req)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+	})
+})
+
+describe("Child", () => {
+	const child = {
+		id: 1,
+		household_id: "a12345",
+		username: "Bobby",
+		child: true,
+		points: 60,
+	}
+
+	test("Insert is called from the middleware", () => {
+		const req = {
+			body: child,
+			path: `/assign/${child.id}`,
+			params: {
+				id: child.id,
+			}
+		}
+		agent.post(req.path)
+			.send(req)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+	})
+	test("Remove is called from the middleware", () => {
+		const req = {
+			body: child,
+			path: `/unassign/${child.id}`,
+			params: {
+				id: child.id,
 			}
 		}
 		agent.post(req.path)
